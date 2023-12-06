bf818cfd97aa30899f9a7bddc7ed305df58071e5
bibekgurung9 <100699318+bibekgurung9@users.noreply.github.com>
2023-12-06 16:36:58 +0545

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+//middleware function represents the incoming requests
+export function middleware(request: NextRequest){
+
+    const path = request.nextUrl.pathname;
+
+    //retrieves the path from the request url, check public path 
+    const isPublicPath = path ==="/login" || path ==="/signup"
+
+    //retrieves the 'token' from the request coookies
+    const token = request.cookies.get("token")?.value || ""
+
+    //if the path is a public path and there is a token, redirects to root URL 
+    if(isPublicPath && token){
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+    
+    //if the path is not a public path and there is no token, redirects to login page 
+    if(!isPublicPath && !token){
+        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    }
+}
+
+//add here, so it is tracked
+export const config = {
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/signup',
+    ]
+}

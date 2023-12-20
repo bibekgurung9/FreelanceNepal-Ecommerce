ca25b14cd4c82b0378bb0bee6e6c016c2fd64970
bibekgurung9 <100699318+bibekgurung9@users.noreply.github.com>
2023-12-20 22:03:32 +0545

    navbar & dropdown

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
-import NavBar from "@/components/NavBar/NavBar";
+import NavBar from "@/components/NavBar";
 
 
 export default function LoginPage(){

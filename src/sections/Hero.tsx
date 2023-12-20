b3976e9ea729e569f0d76c62f1f16eaa83bfe783
bibekgurung9 <100699318+bibekgurung9@users.noreply.github.com>
2023-12-20 22:50:20 +0545

    hero section css

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,40 +2,32 @@ import React from 'react'
 import Buttons from '../components/Buttons/Buttons'
 import { statistics } from '@/constants'
 import Image from 'next/image'
-import heroImage from '@/assets/images/heroImage.jpg'
 
 const Hero = () => {
   return (
     <section
         id="home"
-        className='w-full flex xl:flex-row flex-col justify-center min-h-screen max-container gap-10 back'
-        style={{
-          backgroundImage: `url(${heroImage.src})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-        }}>
-            <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 bg-transparent'>
+        className='w-full flex xl:flex-row flex-col justify-center min-h-screen max-container gap-4'>
+            <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-12 '>
 
-            <h1 className='bg-transparent text-white text-3xl font-bold'>Work Should Be Done</h1>
-            <p className='bg-transparent text-white text-2xl font-bold'>Find a Client. Find a Freelancer. Find The Best. Do Your Best.</p>
+            <h1 className=' text-black text-4xl font-bold bg'>Work Should Be Done</h1>
+            <p className=' text-black text-2xl font-bold'>Find a Client. Find a Freelancer. Find The Best. Do Your Best.</p>
               
-              <div className='flex gap-16 mt-6 bg-transparent'>
+              <div className='flex gap-8 mt-6'>
               <Buttons 
-                  label="I am a Freelancer" 
+                  label="I'm a Freelancer" 
                   iconUrl=""
                   />
               <Buttons 
-                  label="I am a Client" 
+                  label="I'm a Client" 
                   iconUrl=""
                   />
               </div>
-
-                <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16 bg-transparent'>
+                <div className='flex justify-start items-start flex-wrap w-full mt-10 gap-16'>
                 {statistics.map((stat) => (
-                  <div key={stat.label} className='bg-transparent'>
-                    <p className='bg-transparent text-white text-xl'>{stat.value}</p>
-                    <p className='bg-transparent text-white text-xl'>{stat.label}</p>
+                  <div key={stat.label} className=''>
+                    <p className=' text-black text-xl font-bold'>{stat.value}</p>
+                    <p className=' text-black text-xl font-bold'>{stat.label}</p>
                   </div>
                 ))}
                 </div>

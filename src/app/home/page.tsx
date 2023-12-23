5b8cf893505690182442ea52107468d13e58b034
bibekgurung9 <100699318+bibekgurung9@users.noreply.github.com>
2023-12-23 22:44:22 +0545

    for clients section added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Hero from "@/sections/Hero";
 import NavBar from "@/components/NavBar";
-import TrustedBy from "@/sections/TrustedBy";
 import Services from "@/sections/Services";
 import ForClients from "@/sections/ForClients";
 
@@ -11,16 +10,13 @@ export default function HomePage() {
             <section className="">
                 <NavBar />
             </section>
-            <section className="padding-x">
+            <section className="padding-x bg-blue-500">
                 <Hero />
             </section>
             <section className="padding-x py-10">
             <   Services />
             </section>
-            <section className="bg-pale-blue-padding">
-                //
-            </section>
-            <section className="padding-x sm:py-32 py-16 w-full">
+            <section className="padding-x sm:py-1 py-16 w-full bg-blue-500">
                 <ForClients/>
             </section>
             <section className="padding-x sm:py-32 py-16 w-full">
--- src/sections/ForClients.tsx
+++ src/sections/ForClients.tsx
@@ -7,33 +7,29 @@ const ForClients = () => {
     <section
     id="home"
     className='w-full flex xl:flex-row flex-col justify-center min-h-screen max-container gap-4 bg-blue-500'>
-        <h1 className=' text-white font-sans text-5xl font-bold bg-blue-500'>Are You In Need Of A Freelancer?</h1>
-        <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-12  bg-blue-500 text-white'>
+        <h1 className=' text-white font-sans text-4xl font-bold bg-blue-500'>Are You In Need Of A Talented Freelancer?</h1>
+        <div className='relative flex flex-col justify-center items-start w-full bg-blue-500 text-white'>
 
-        <p className='mt-1 text-2xl font-bold  bg-blue-500 text-white'>Find one of the best talents in the industry.<br />If you don't have an account with us, you can sign up from here.</p>
+        <p className='mt-1 text-1xl font-bold bg-blue-500 text-white'>Hire one of the best talents in the industry.<br />If you don't have already have an account with us, you can sign up from here.</p>
         <div className='flex gap-8 mt-6'>
+            <a href="/signup">
             <Buttons 
-                label="Create A Account" 
-                iconUrl=""
+                label="Create A New Account" 
+                iconUrl=''
                 />
+            </a>
         </div>
 
-        <p className=' text-1xl font-bold text-black'>If you already have a account with us click the button below to search for one of the finest talents in the industry you are looking for.</p>          
+        <p className=' text-1xl font-bold text-white'>If you already have a account with us click the button below to search for one of the finest talents in the industry you are looking for.</p>          
         <div className='flex gap-8 mt-6 '>
+            <a href="/search">
             <Buttons 
                 label="I'm in search for talent" 
                 iconUrl=""
                 />
+              </a>
         </div>
 
-            <div className='flex justify-start items-start flex-wrap w-full mt-10 gap-16'>
-            {statistics.map((stat) => (
-              <div key={stat.label} className=''>
-                <p className=' text-black text-xl font-bold'>{stat.value}</p>
-                <p className=' text-black text-xl font-bold'>{stat.label}</p>
-              </div>
-            ))}
-            </div>
         </div>
 </section>
   )
